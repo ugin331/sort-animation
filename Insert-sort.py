8cefd2c248ff7b7af40b3a16698a4714ae2e5291
Absolutely nothing you need to be concerned with <yuanren.li@gmail.com>
2018-01-06 12:26:11 -0800

    Update Insert-sort.py

--- Insert-sort.py
+++ Insert-sort.py
@@ -8,12 +8,12 @@ for i in reversed(range(len(list1))):
     print(i, ": ", list1[i])
 for i in range(len(list1)):
     #set a temporary variable to go through each number in the list, starting on the one in pos i, then going down.
-    tmp_var = i
-    #do a while loop to run through each previous number, and it stops when either tmp_var becomes 0, or if the current number becomes less than the one before it
-    while tmp_var > 0 and list1[tmp_var]> list1[tmp_var-1]:
+    j = i
+    #do a while loop to run through each previous number, and it stops when either j becomes 0, or if the current number becomes less than the one before it
+    while j > 0 and list1[j] > list1[j-1]:
         #swap numbers
-        list1[tmp_var], list1[tmp_var-1] = list1[tmp_var-1], list1[tmp_var]
+        list1[j], list1[j-1] = list1[j-1], list1[j]
         #move one down the list
-        tmp_var -= 1
+        j -= 1
         print(list1)
 print(list1[::-1])
