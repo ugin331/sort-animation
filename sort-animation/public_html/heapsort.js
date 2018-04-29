d2960094532dcafb71b7b156cacb76d5a1cffcaf
yuanr <yuanr@Ruby>
2018-04-29 15:13:56 -0700

    so heapify isn't heapify anymore, it's sorting

--- sort-animation/public_html/heapsort.js
+++ sort-animation/public_html/heapsort.js
@@ -6,16 +6,16 @@
 
 var array = [1000, 4000, 6000, 3000, 2000, 5000, 7000, 10000, 8000, 9000];
 var newarray = [];
-function leftroot(i){
+function left_child(i){
     var reet = 2*i+1;
     return reet;
 }
-function rightroot(i){
+function right_child(i){
     var reeto = 2*i+2;
     return reeto;
 }
-function detectleftroot(arraypos){
-    if(leftroot(arraypos)>array.length-1){
+function detectleft_child(arraypos){
+    if(left_child(arraypos)>array.length-1){
         return false;
     }
     else{
@@ -29,23 +29,26 @@ function swap(swap1, swap2){
     array[swap1] = nexvar; //sets the original array spot with the swapped one
     console.log(array);
 }
-function heapify(rootnum){
+function sorting(rootnum){
     console.log(rootnum);
-    while(detectleftroot===true){
-        detectleftroot(rootnum);
-        console.log(detectleftroot(rootnum));
-        if(detectleftroot===null){
+    while(detectleft_child===true){
+        detectleft_child(rootnum);
+        console.log(detectleft_child(rootnum));
+        if(detectleft_child===null){
             break;
         }
-        heapify(leftroot(rootnum));
-        heapify(leftroot(rootnum));
+        heapify(left_child(rootnum));
+        heapify(left_child(rootnum));
     }
     SiftDown(rootnum);
     console.log(array);
 }
+function heapify(placeholder){
+    
+}
 function SiftDown(i){
-        var left = leftroot(i);
-        var right = rightroot(i);
+        var left = left_child(i);
+        var right = right_child(i);
     if(array[left] > array[i]){
         swap(left, i);
     }
@@ -56,4 +59,3 @@ function SiftDown(i){
         console.log("nothing to do");
     }
 }
-heapify(0);
